1bdae42e685fef10b578980adbb2b7729b7fb32b
Tomas Garcia <910tomy910@uma.es>
2021-01-31 12:56:56 +0100

    "Added x+y to Excel. Fixes in Main.java"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -39,6 +37,9 @@ public class Main {
             System.out.println(estado + " " + expresion + " " + instruccion + " " + transicion);
             startRow++;
         }
+        System.out.println();
+        System.out.println("-------------");
+        System.out.println();
     }
 
     public static void executeMT(){
@@ -57,13 +58,13 @@ public class Main {
             transicion = tablaIT.getTransicion(estadoActual, expresionCinta);
             System.out.println("Transicion " + transicion);
 
-            switch(instruccion){
-                case "L": mt.left(); break;
-                case "R": mt.right();  break;
-                case "*": mt.putExpresionCinta('*');  break;
-                case "I": mt.putExpresionCinta('I');  break;
-                case "H": mt.putExpresionCinta('H'); break;
-                default: break;
+            switch (instruccion) {
+                case "L" -> mt.left();
+                case "R" -> mt.right();
+                case "*" -> mt.putExpresionCinta('*');
+                case "I" -> mt.putExpresionCinta('I');
+                case "H" -> mt.putExpresionCinta('H');
+                default -> throw new RuntimeException("Caracter Invalido");
             }
             tablaIT.putEstadoActual(transicion);
 
@@ -72,25 +73,14 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        FileInputStream fis = new FileInputStream(new File(System.getProperty("user.dir") + "/MT.xlsx"));
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/MT.xlsx");
         XSSFWorkbook wb = new XSSFWorkbook(fis);
         XSSFSheet sheet = wb.getSheetAt(0);
 
         mt = createMT(sheet);
 
         createTabla(sheet);
-/*
-        System.out.println(estados.transicion.values());
-        System.out.println(mt.getExpresionCinta());
-        System.out.println(estados.getEstadoActual());
-        System.out.println(estados.getIntruccion(estados.getEstadoActual(), mt.getExpresionCinta()));
-        System.out.println(estados.instruccion.get(estados.getEstadoActual()).get("I"));
-        //System.out.println(estados.getTransicion(estados.getEstadoActual(),mt.getExpresionCinta()));
-           mt.right();
-        mt.right();
-        mt.right();
-        mt.printMT();
-*/
+
         executeMT();
     }
 }
